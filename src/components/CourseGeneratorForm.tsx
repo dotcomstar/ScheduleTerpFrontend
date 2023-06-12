082bf2036625e91d8bbc8cf6ed71a59316b3fb72
dotcomstar <gamestar109@live.com>
2023-06-12 02:34:20 -0700

    Add formvalues array

--- src/components/CourseGeneratorForm.tsx
+++ src/components/CourseGeneratorForm.tsx
@@ -1,15 +1,24 @@
-import { Box, Button, Stack } from "@mui/material";
+import { Button, Stack } from "@mui/material";
 import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 import CourseSelector from "./CourseSelector";
+
+export type FormValues = {
+  course1: string;
+  course2?: string;
+  course3?: string;
+  course4?: string;
+  course5?: string;
+};
 
 const CourseGeneratorForm = () => {
   const courseIds = [1, 2, 3, 4];
   const [data, setData] = useState<string[]>([]);
-  const { handleSubmit, register, reset, control } = useForm();
+  const { handleSubmit, register, reset, control } = useForm<FormValues>();
+  const onSubmit: SubmitHandler<FormValues> = (data) => console.log(data);
 
   return (
-    <form onSubmit={handleSubmit((data) => console.log(data))} className="form">
+    <form onSubmit={handleSubmit(onSubmit)} className="form">
       <Stack
         direction="column"
         justifyContent="center"
@@ -23,6 +32,7 @@ const CourseGeneratorForm = () => {
         <Button
           variant="contained"
           color="secondary"
+          type="submit"
           sx={{
             width: { xs: "80%", sm: "60%" },
             px: { xs: "10%", sm: "20%" },
--- src/components/CourseSelector.tsx
+++ src/components/CourseSelector.tsx
@@ -4,11 +4,12 @@ import parse from "autosuggest-highlight/parse";
 import match from "autosuggest-highlight/match";
 import useSearch, { SearchResult } from "../hooks/useSearch";
 import CourseDetails from "./CourseDetails";
-import { Control } from "react-hook-form";
+import { Control, Controller } from "react-hook-form";
+import { FormValues } from "./CourseGeneratorForm";
 
 interface Props {
   formId: number;
-  control: Control;
+  control: Control<FormValues, any>;
 }
 
 const CourseSelector = ({ formId, control }: Props) => {
@@ -16,65 +17,67 @@ const CourseSelector = ({ formId, control }: Props) => {
   const options = useSearch(inputValue);
 
   return (
-    <Autocomplete
-      sx={{
-        width: "100%",
-        px: { xs: "10%", sm: "20%" },
-      }}
-      onInputChange={(_, value) => {
-        setInputValue(value);
-      }}
-      options={options?.data || []}
-      autoSelect={true}
-      filterOptions={(o: SearchResult[]) => o}
-      autoHighlight={true}
-      disableListWrap={true}
-      getOptionLabel={(option: SearchResult) => option.name}
-      // getOptionSelected={(option, value) => option.name === value.name}
-      isOptionEqualToValue={(option, value) => option.name === value.name}
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          label={"Course " + formId.toString()}
-          variant="outlined"
-          key={formId}
-          InputProps={{
-            ...params.InputProps,
-            endAdornment: (
-              <>
-                {/* <CourseDetails courseName={selectedCourse} /> */}
-                {params.InputProps.endAdornment}
-              </>
-            ),
-          }}
-          type="text"
-        />
-      )}
-      renderOption={(props, option, { inputValue }) => {
-        const matches = match(option.name, inputValue, { insideWords: true });
-        const parts = parse(option.name, matches);
+    <>
+      <Autocomplete
+        sx={{
+          width: "100%",
+          px: { xs: "10%", sm: "20%" },
+        }}
+        onInputChange={(_, value) => {
+          setInputValue(value);
+        }}
+        options={options?.data || []}
+        autoSelect={true}
+        filterOptions={(o: SearchResult[]) => o}
+        autoHighlight={true}
+        disableListWrap={true}
+        getOptionLabel={(option: SearchResult) => option.name}
+        // getOptionSelected={(option, value) => option.name === value.name}
+        isOptionEqualToValue={(option, value) => option.name === value.name}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label={"Course " + formId.toString()}
+            variant="outlined"
+            key={formId}
+            InputProps={{
+              ...params.InputProps,
+              endAdornment: (
+                <>
+                  {/* <CourseDetails courseName={selectedCourse} /> */}
+                  {params.InputProps.endAdornment}
+                </>
+              ),
+            }}
+            type="text"
+          />
+        )}
+        renderOption={(props, option, { inputValue }) => {
+          const matches = match(option.name, inputValue, { insideWords: true });
+          const parts = parse(option.name, matches);
 
-        return (
-          <li {...props} key={option.name}>
-            <div>
-              {parts.map((part, index) => (
-                <span
-                  key={index}
-                  style={{
-                    fontWeight: part.highlight ? 700 : 400,
-                  }}
-                >
-                  {part.text}
-                </span>
-              ))}
-              {option.type === "course" && (
-                <CourseDetails courseName={option.name} />
-              )}
-            </div>
-          </li>
-        );
-      }}
-    />
+          return (
+            <li {...props} key={option.name}>
+              <div>
+                {parts.map((part, index) => (
+                  <span
+                    key={index}
+                    style={{
+                      fontWeight: part.highlight ? 700 : 400,
+                    }}
+                  >
+                    {part.text}
+                  </span>
+                ))}
+                {option.type === "course" && (
+                  <CourseDetails courseName={option.name} />
+                )}
+              </div>
+            </li>
+          );
+        }}
+      />
+    </>
   );
 };
 
