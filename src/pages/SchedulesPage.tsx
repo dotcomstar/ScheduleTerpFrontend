8527a1df025f8dbcd98cb1a0c3cfda851ae9a955
dotcomstar <gamestar109@live.com>
2023-06-22 17:48:36 -0700

    Add basic calendar

--- src/pages/SchedulesPage.tsx
+++ src/pages/SchedulesPage.tsx
@@ -1,5 +1,42 @@
 import { Grid, Paper, styled } from "@mui/material";
 import useCoursesStore from "../courses/store";
+import {
+  AppointmentModel,
+  ViewState,
+  SchedulerDateTime,
+} from "@devexpress/dx-react-scheduler";
+import {
+  Scheduler,
+  WeekView,
+  Appointments,
+  Resources,
+} from "@devexpress/dx-react-scheduler-material-ui";
+import React from "react";
+
+const appointments: Array<AppointmentModel> = [
+  {
+    startDate: "2018-10-31T10:00",
+    endDate: "2018-10-31T11:15",
+    title: "Meeting",
+    type: "private",
+  },
+  {
+    startDate: "2018-10-31T07:30",
+    endDate: "2018-10-31T09:00",
+    title: "Go to a gym",
+    type: "work",
+  },
+];
+const resources = [
+  {
+    fieldName: "type",
+    title: "Type",
+    instances: [
+      { id: "private", text: "Private", color: "#EC407A" },
+      { id: "work", text: "Work", color: "#7E57C2" },
+    ],
+  },
+];
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -17,13 +54,27 @@ const SchedulesPage = () => {
   ) {
     return <p>No courses selected!</p>;
   }
+  const [currentDate, setCurrentDate] =
+    React.useState<SchedulerDateTime>("2018-10-31");
+
   return (
     <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
       {courses.map(
         (c, i) =>
           c.name !== "" && (
             <Grid item xs={6}>
-              <Item key={i}>{c.name}</Item>
+              <Paper>
+                <Scheduler data={appointments}>
+                  <ViewState
+                    currentDate={currentDate}
+                    onCurrentDateChange={setCurrentDate}
+                  />
+                  <WeekView startDayHour={7} endDayHour={12} />
+
+                  <Appointments />
+                  <Resources data={resources} />
+                </Scheduler>
+              </Paper>
             </Grid>
           )
       )}
