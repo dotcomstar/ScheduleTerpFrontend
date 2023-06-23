57b3a190650af8ba98a00a71e0ab2a7061d472d7
dotcomstar <gamestar109@live.com>
2023-06-22 17:08:26 -0700

    Add grid to schedule page

--- src/pages/SchedulesPage.tsx
+++ src/pages/SchedulesPage.tsx
@@ -1,13 +1,27 @@
+import { Grid, Paper, styled } from "@mui/material";
 import useCoursesStore from "../courses/store";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 const SchedulesPage = () => {
   const courses = useCoursesStore((s) => s.courses);
   return (
-    <div>
-      {courses.map((c, i) => (
-        <p key={i}>{c.name}</p>
-      ))}
-    </div>
+    <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+      {courses.map(
+        (c, i) =>
+          c.name !== "" && (
+            <Grid item xs={6}>
+              <Item key={i}>{c.name}</Item>
+            </Grid>
+          )
+      )}
+    </Grid>
   );
 };
 
