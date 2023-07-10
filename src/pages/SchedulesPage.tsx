e4fc8ff6b0019e2458a67e81001d9b4e534ad4d6
dotcomstar <gamestar109@live.com>
2023-07-10 10:56:33 -0700

    Display a full length calendar

--- src/pages/SchedulesPage.tsx
+++ src/pages/SchedulesPage.tsx
@@ -1,10 +1,6 @@
 import { Grid, Paper } from "@mui/material";
 import useCoursesStore from "../courses/store";
-import {
-  AppointmentModel,
-  ViewState,
-  SchedulerDateTime,
-} from "@devexpress/dx-react-scheduler";
+import { ViewState, SchedulerDateTime } from "@devexpress/dx-react-scheduler";
 import {
   Scheduler,
   WeekView,
@@ -17,42 +13,11 @@ import getAppointments from "../courses/getAppointments";
 import CustomAppointmentTooltip from "../components/CustomAppointmentTooltip";
 import useSchedules from "../hooks/useSchedules";
 
-const sectionTimes = {
-  enes100: ["TuTh 2:00pm-3:15pm", "ENES100"] as const,
-  math100: ["MWF 10:00am-10:50am", "MATH100"] as const,
-};
-const course1 = getAppointments(...sectionTimes.enes100, "CSIC200", "work");
-const course2 = getAppointments(...sectionTimes.math100, "CSIC200", "private");
-
 const firstDate = new Date("2023-08-29T08:00");
 const ThanksgivingDates = {
   startDate: new Date("2023-11-23T00:00"),
   endDate: new Date("2023-11-27T23:59"),
 };
-
-const appointments: Array<AppointmentModel> = [
-  {
-    startDate: "2018-10-31T10:00",
-    endDate: "2018-10-31T11:15",
-    title: "Meeting",
-    type: "private",
-  },
-  {
-    startDate: "2018-10-31T07:30",
-    endDate: "2018-10-31T09:00",
-    title: "Go to a gym",
-    type: "work",
-  },
-  {
-    startDate: firstDate,
-    endDate: "2023-08-29T10:00",
-    title: "Test class",
-    type: "work",
-    location: "What",
-  },
-  ...course1,
-  ...course2,
-];
 
 const resources = [
   {
@@ -84,7 +49,7 @@ const SchedulesPage = () => {
     schedule.map((course, i) =>
       getAppointments(
         course.lectures,
-        course.class_name,
+        course.class_name + " - " + course.section_num,
         "Section " + course.section_num,
         resources[0].instances[i % resources[0].instances.length].id
       )
@@ -95,13 +60,13 @@ const SchedulesPage = () => {
     React.useState<SchedulerDateTime>("2023-08-29");
 
   return (
-    <Grid container rowSpacing={4} columnSpacing={{ xs: 2, sm: 3, md: 4 }}>
+    <Grid container rowSpacing={4} columnSpacing={{ xs: 0, md: 1 }}>
       {courses.map(
         (schedule, i) =>
           i < 20 && (
             <Grid item xs={6} key={i}>
-              <Paper>
-                <Scheduler data={schedule.flat()} height={600}>
+              <Paper sx={{ marginX: { xs: 1, sm: 2, md: 3 }, border: 1 }}>
+                <Scheduler data={schedule.flat()} height="10%">
                   <ViewState
                     currentDate={currentDate}
                     onCurrentDateChange={setCurrentDate}
